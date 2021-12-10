4be5afa5bbcfb9a85d9111829802f726fef590b3
chrisnabayuaji <chrisnabayuaji@gmail.com>
2021-12-10 14:02:11 +0700

    tambah

--- index.php
+++ index.php
@@ -10,7 +10,19 @@ try {
 
   /* Print a "Hello world" receipt" */
   $printer = new Printer($connector);
-  $printer->text("Hello World!\n");
+  $printer->text("2109220197              PendBPJS Printer 22 Sep 2021");
+  $printer->text("----------------------------------------------------");
+  $printer->text("FILE REKAM MEDIS");
+  $printer->text("----------------------------------------------------");
+  $printer->text("Jenis Pasien : BPJS");
+  $printer->text("Status Pasien : Lama");
+  $printer->text("No. RM : 088521");
+  $printer->text("Nama Pasien : Sri Sulanjari");
+  $printer->text("Alamat : Kemabaran 01/02");
+  $printer->text("Alamat : Kebumen");
+  $printer->text("Poli Tujuan : Klinik jantung dan Pembuluh darah");
+  $printer->text("Tanggal masuk : 22 September 2021");
+  $printer->text("tanggal Terakhir masuk");
   // $printer->cut();
 
   /* Close printer */
