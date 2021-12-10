4b83364b01a2c7e9d30a551ca00a9cb365500457
chrisnabayuaji <chrisnabayuaji@gmail.com>
2021-12-10 13:45:42 +0700

    test

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ require __DIR__ . '/vendor/autoload.php';
 use Mike42\Escpos\PrintConnectors\FilePrintConnector;
 use Mike42\Escpos\Printer;
 
-$connector = new FilePrintConnector("php://stdout");
+// $connector = new FilePrintConnector("php://stdout");
+$connector = new WindowsPrintConnector("192.168.1.113/epson\ lq-310\ escp2");
 $printer = new Printer($connector);
 $printer->text("Hello World!\n");
 $printer->cut();
