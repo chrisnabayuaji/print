5d9d9c39437a2379cff7ae2c7e83bc4050d7c0c1
chrisnabayuaji <chrisnabayuaji@gmail.com>
2021-12-10 14:16:05 +0700

    update

--- index.php
+++ index.php
@@ -40,6 +40,7 @@ try {
   $printer->text("tanggal Terakhir masuk\n");
   $printer->text("tanggal Terakhir masuk\n");
   $printer->text("Printed at " . date('d M Y H:i:s') . "\n");
+  $printer->text("\n");
   $printer->feed(10);
   // $printer->cut();
 
